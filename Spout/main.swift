d1d6509eb6e468b16611528560b17f6045f39bd1
Tim Wolff <timtwotoes@me.com>
2020-10-06 19:22:00 +0200

    Initial Commit

---
+++ Spout/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Spout
+//
+//  Created by Tim Wolff on 06/10/2020.
+//  Copyright © 2020 Tim Wolff. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
